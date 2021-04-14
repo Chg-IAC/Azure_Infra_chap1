596348d72610b86edf0d868b0b93bc789e23e08b
Philippe <52935095+Chg-IAC@users.noreply.github.com>
2021-04-15 00:55:05 +0200

    Update variable_azure.tf

--- variable_azure.tf
+++ variable_azure.tf
@@ -1,18 +1,18 @@
 variable "subscription_id" {
   description = "Id of subscription of azure's provider."
-  default     = "feb54016-2be4-4232-80fe-8b20a5d5cbf5"
+  default     = "id_sub"
 }
 variable "client_id" {
   description = "Id of client Id of azure's provider."
-  default     = "f1f75ca5-e824-4bea-b299-62a0fe60aaed"
+  default     = "id_client"
 }
 variable "client_secret" {
   description = "Id of client secret Id of azure's provider."
-  default     = ".WSw55HRm9cN_Tu4-7mi4oG7zCocXjvF.-"
+  default     = "id_client_secret"
 }
 variable "tenant" {
   description = "Id of tenant Id of azure's provider."
-  default     = "b26ed726-0ada-415b-9cac-524a317e13b3"
+  default     = "id_tenant"
 }
 variable "resource_group" {
   description = "The name of your Azure Resource Group."
